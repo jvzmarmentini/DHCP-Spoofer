ef223bdad6bbcd434f628cc9fd610cb4569e81da
grpanho <grpanho@hotmail.com>
2022-08-30 18:03:01 -0300

    MAC addresses and network protocol captured

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 from socket import *
 from struct import *
 
+def format_mac(mac_addr):
+    return ':'.join(mac_addr.hex()[i:i+2] for i in range(0, len(mac_addr.hex()), 2))
+
 s = socket(AF_PACKET, SOCK_RAW, ntohs(0x0003)) # ETH_P_ALL = 0x0003
  
 while True:
     packet = s.recvfrom(4096) # For best match with hardware and network realities, bufsize should be a relatively small power of 2, for example, 4096. 
-    print(packet, end="\n\n")
+#    print(packet, end="\n\n")
+
+    eth_header = packet[0][0:14]
+    eth = unpack('!6s6sH', eth_header)
+    network_proto = ntohs(eth[2])
+    print('Destination MAC: ' + format_mac(packet[0][0:6]) + ' Source MAC: ' + format_mac(packet[0][6:12]) + ' Protocol: ' + str(network_proto))
