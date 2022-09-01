2ea8d81ea3c905d0dfaad5291399cda48fd61403
grpanho <grpanho@hotmail.com>
2022-08-31 22:11:07 -0300

    IPv4 almost ready

--- main.py
+++ main.py
@@ -14,9 +14,22 @@ while True:
     eth_header = packet[0][0:14]
     eth = unpack('!6s6sH', eth_header)
     network_proto = ntohs(eth[2])
-    print('Destination MAC: ' + format_mac(eth[0]) + ' Source MAC: ' + format_mac(eth[1]) + ' Protocol: ' + str(network_proto))
+#    print('Destination MAC: ' + format_mac(eth[0]) + ' Source MAC: ' + format_mac(eth[1]) + ' Protocol: ' + str(network_proto))
 
     if network_proto == 8 :
         ipv4_header = packet[0][14:34]
-        ipv4 = unpack("!12s4s4s", ipv4_header)
-        print("Source IP: " + inet_ntoa(ipv4[1]) + " Destination IP: " + inet_ntoa(ipv4[2]))
+        ipv4 = unpack("!BBHHHBBH4s4s", ipv4_header)
+        
+        version_ihl = ipv4[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
+ 
+        ipv4_length = ihl * 4
+ 
+        ttl = ipv4[5]
+        transport_protocol = ipv4[6]
+        source_addr = inet_ntoa(ipv4[8]);
+        dest_addr = inet_ntoa(ipv4[9]);
+ 
+        print('Version: ' + str(version) + ' IP Header Length: ' + str(ihl) + ' TTL: ' + str(ttl) + ' Protocol: ' + str(transport_protocol) + ' Source Address: ' + str(source_addr) + ' Destination Address: ' + str(dest_addr))
+ 
