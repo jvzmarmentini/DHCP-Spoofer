1779067d08c23e1583dc37f55777515e1c4dcb3f
Gabriel Reis Panho <gabriel.panho@kinghost.com.br>
2022-11-01 21:41:36 -0300

    udp packet sent started

--- spoofer.py
+++ spoofer.py
@@ -1,5 +1,6 @@
 import socket as s
 import netifaces as netif
+import ipaddress as ip
 from typing import Dict 
 import time #temporário
 
@@ -26,7 +27,6 @@ class Spoofer:
 
             if 2 in addresses.keys():
                 if iface.startswith('wlp') or iface.startswith('enp'):
-                    print(iface)        
                     mask = addresses[2][0]['netmask']
 
         if inBytes:
@@ -35,22 +35,30 @@ class Spoofer:
         return mask
     
     @staticmethod
-    def write_pkg(dhcp_offer_header : Dict): #TODO: FALTAM OUTROS CAMPOS DO HEADER ALÉM DAS OPTIONS
+    def write_offer(dhcp_offer_header : Dict): #TODO: FALTAM OUTROS CAMPOS DO HEADER ALÉM DAS OPTIONS
         print(dhcp_offer_header)
-        dhcp_offer_header[1] =   {'length': 4, 'res': Spoofer.getNetmask(True)} # ok
-        dhcp_offer_header[3] =   {'length': 4, 'res': Spoofer.getIP(True)}      # ok
-        dhcp_offer_header[6] =   {'length': 4, 'res': Spoofer.getIP(True)}      # ok
-        dhcp_offer_header[51] =  {'length': 4, 'res': 3600}                     # ok
-        dhcp_offer_header[53] =  {'length': 1, 'res': 0x2}                      # ok  
-        dhcp_offer_header[54] =  {'length': 4, 'res': Spoofer.getIP(True)}      # ok
-        dhcp_offer_header[58] =  {'length': 4, 'res': 1800}                     # ok
-        dhcp_offer_header[59] =  {'length': 4, 'res': 3150}                     # ok 
-        dhcp_offer_header[0] =   {'length': 4, 'res': 0x00000}           
-        dhcp_offer_header[255] = {'length': 4, 'res': 0x00}
+        dhcp_offer_header['op']     =   2
+        dhcp_offer_header['yiaddr'] =   s.inet_aton('10.132.249.253')
+        dhcp_offer_header[1]        =   {'length': 4, 'res': Spoofer.getNetmask(True)}  # ok
+        dhcp_offer_header[3]        =   {'length': 4, 'res': Spoofer.getIP(True)}       # ok
+        dhcp_offer_header[6]        =   {'length': 4, 'res': Spoofer.getIP(True)}       # ok
+        dhcp_offer_header[51]       =   {'length': 4, 'res': 3600}                      # ok
+        dhcp_offer_header[53]       =   {'length': 1, 'res': 0x2}                       # ok  
+        dhcp_offer_header[54]       =   {'length': 4, 'res': Spoofer.getIP(True)}       # ok
+        dhcp_offer_header[58]       =   {'length': 4, 'res': 1800}                      # ok
+        dhcp_offer_header[59]       =   {'length': 4, 'res': 3150}                      # ok 
+        dhcp_offer_header[0]        =   {'length': 4, 'res': 0x00000}           
+        dhcp_offer_header[255]      =   {'length': 4, 'res': 0x00}
 
         return dhcp_offer_header
 
     @staticmethod
     def spoof(dhcp_discover_header : Dict):
-        offer_header = Spoofer.write_pkg(dhcp_discover_header)
-        #TODO: ABRIR SOCKET E ENVIAR PACOTE AO SERVIDOR
+        offer_header = Spoofer.write_offer(dhcp_discover_header)
+        broadcast = ip.IPv4Network(Spoofer.getIP() + '/' + Spoofer.getNetmask(), False).broadcast_address
+
+        dport = 68
+        udp = s.socket(s.AF_INET, s.SOCK_DGRAM)
+        dest = (broadcast, dport)
+        udp.sendto(offer_header.encode("utf-8"), dest) #TODO: CONVERTER DICIONÁRIO (HEADER) PRA BYTES E ENVIAR 
+        udp.close()
